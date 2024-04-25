10fd416a4e15343f4a66e9682142d486824418b5
Kunal Thool <thoolkunal93@gmail.com>
2024-04-25 05:54:55 +0530

    removed unused variables

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,50 @@
+"use strict";
+
 import { factory } from "./factory";
 
-let count = factory(0, 1);
+const error = document.getElementById("error");
+
+const start_at_control = document.getElementById("start_at");
+
+const step_control = document.getElementById("step");
+
+const count_button = document.querySelector(".count_button");
+
+const current_count = document.querySelector(".current_count");
+
+
+start_at_control.addEventListener("change", () => {
+  update_count_and_reset_counter();
+});
+
+step_control.addEventListener("change", () => {
+  update_count_and_reset_counter();
+});
+
+count_button.addEventListener("click", update_count);
 
 function update_count_and_reset_counter() {
   let start = parseInt(start_at_control.value);
-  let end = parseInt(step_control.value);
+  let end   = parseInt(step_control.value);
 
   if (isNaN(start) || isNaN(end)) {
       error.innerHTML = "Please enter valid number.";
       return;
   }
 
-  const count = factory();
-  
-  current_count.innerHTML = count();
+  current_count.innerHTML = factory(start, end)();
 
   error.innerHTML = "";
 }
 
-const error = document.getElementById("error");
 
-const start_at_control = document.getElementById(
-  "start_at",
-) as HTMLInputElement;
-
-const step_control = document.getElementById("step") as HTMLInputElement;
-
-start_at_control?.addEventListener("change", () => {
-  update_count_and_reset_counter();
-});
-
-step_control?.addEventListener("change", () => {
-  update_count_and_reset_counter();
-});
-
-const count_button = document.querySelector(
-  ".count_button",
-) as HTMLButtonElement;
-
-const current_count = document.querySelector(
-  ".current_count",
-) as HTMLSpanElement;
-
+/**
+ * update_count for button click
+ */
 function update_count() {
   const count = factory(parseInt(current_count.innerHTML),  parseInt(step_control.value));
   current_count.innerHTML = count();
 }
 
-count_button.addEventListener("click", update_count);
-
+//Calling for page load
+update_count_and_reset_counter();
